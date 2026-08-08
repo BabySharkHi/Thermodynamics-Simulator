5fab8f21f590e1343cc88532025da984bf395733
BabySharkHi <babysharkish@gmail.com>
2026-08-08 04:08:53 -0400

    Added better inputs (temp still is buggy)

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const canvas = document.getElementById("container");
 const ctx = canvas.getContext("2d");
 const temperatureDisplay = document.getElementById("temperature");
 const areaDisplay = document.getElementById("area");
+const collisionDisplay = document.getElementById("collisions");
+const impulsePerCollisionDisplay = document.getElementById("impulse-per-collision");
 const pressureDisplay = document.getElementById("pressure");
 const moleculeCountDisplay = document.getElementById("molecule-count");
 
 const pausePlayButton = document.getElementById("pause-play-button");
 let isPaused = true;
 
+let curTime = 0.0;
 const simSpeedButton = document.getElementById("sim-speed-button");
 const simSpeedFactors = [0.1,0.2,0.5,1,2,5,10];
 let simSpeedIndex = 3;
 let simSpeedFactor = 1;
 const simSpeedDisplay = document.getElementById("sim-speed");
-
 
 const STARTING_MOLECULES = 200;
 
@@ -27,28 +29,40 @@ const heightSlider = document.getElementById("height-slider");
 const heightScaleDisplay = document.getElementById("height-scale");
 var heightFactor = 1;
 
+let curTemp = 300;
+
+const tempSlider = document.getElementById("temp-slider");
+const tempNumberInput = document.getElementById("temp-number-input");
+const moleSlider = document.getElementById("mole-slider");
+const moleNumberInput = document.getElementById("mole-number-input");
+
 const BOLTZMANN_CONSTANT = 1.380649e-23;
 const NITROGEN_MASS = 4.65e-26;
+const AVAGADRO_NUMBER = 6.022e23;
 
 const SECONDS_PER_FRAME = 1e-5;
-const METERS_PER_PIXEL = 1e-3;
-const SECONDS_PER_WINDOW = 1e-4;
+const METERS_PER_PIXEL = 2.226e-3;
+const SECONDS_PER_WINDOW = 0.01;
 
 const DEGREES_OF_FREEDOM = 2; //degrees of freedom
 
-let curSeconds = 0;
+let numCollisions = 0;
 let totalImpulse = 0;
 const collisionTimes = [];
+let collisionIndex = 0;
 
 class Ball {
-    constructor(x, y, radius = 10, fillColor = "black", vx = 1, vy = 1, mass = 1) {
+    constructor(x, y, radius = 10, fillColor = "black", vx = 1, vy = 1, vz = 1, mass = 1, num = AVAGADRO_NUMBER) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.fillColor = fillColor;
         this.vx = vx;
         this.vy = vy;
+        this.vz = vz;
         this.mass = mass;
+        this.num = num;
+        this.effMass = mass * num;
     }
 }
 
@@ -67,28 +81,41 @@ document.getElementById("remove-molecule")
 
 
 function createMolecule() {
+    let speed = calculateRMSFromTemp();
+    let angle = Math.random() * 2 * Math.PI;
     molecules.push(new Ball(Math.random() * canvas.width,
                             Math.random() * canvas.height,
-                            5,
+                            2,
                             "blue",
-                            random(-500,500),
-                            random(-500,500),
-                            NITROGEN_MASS));
+                            speed * Math.cos(angle),
+                            speed * Math.sin(angle),
+                            0,
+                            NITROGEN_MASS,
+                            AVAGADRO_NUMBER
+                        ));
     N ++;
 }
 
-function addMolecule() {
-    createMolecule();
+function addMolecule(num) {
+    for (let i = 0; i < num; i ++) {
+        createMolecule();
+    }
 }
 
-function removeMolecule() {
-    if (molecules.length == 0) {
+function removeMolecule(num) {
+    if (molecules.length - num <= 0) {
         console.log("error, no more molecules to remove");
     }
     else {
-        let idx = Math.floor(random(0,N));
-        molecules.splice(idx,1);
-        N--;
+        for (let i = 0; i < num; i ++) {
+            let idx = Math.floor(random(0,N));
+            molecules.splice(idx,1);
+            N--;
+        }
+        /*tempRatio = calculateTemperature() / curTemp;
+        for (const b of molecules) {
+            changeSpeedToMatchTemp(b,tempRatio);
+        }*/
     }
 }
 
@@ -102,7 +129,7 @@ function initialize() {
 
 function update() {
 
-    const temperature = calculateTemperature();
+    const temperature = curTemp;
     temperatureDisplay.textContent = 
         `Temperature: ${temperature.toFixed(1)} K`;
 
@@ -117,16 +144,31 @@ function update() {
     moleculeCountDisplay.textContent =
         `Molecule Count: ${N}`;
 
+    collisionDisplay.textContent =
+        `Number of collisions per second: ${numCollisions / SECONDS_PER_WINDOW}`;
+
+    impulsePerCollisionDisplay.textContent =
+        `Impulse per collision: ${totalImpulse / numCollisions}`;
     if (isPaused) {
         requestAnimationFrame(update);
         return;
     }
 
-    curSeconds += getSimulationSpeed();
-    if (curSeconds > SECONDS_PER_WINDOW) {
-        curSeconds -= SECONDS_PER_WINDOW;
-        totalImpulse = 0;
+    curTime += getSimulationSpeed();
+
+    //Remove collisions
+    while (collisionTimes.length > collisionIndex && 
+        collisionTimes[collisionIndex].curTime < curTime - SECONDS_PER_WINDOW) {
+            totalImpulse -= collisionTimes[collisionIndex].impulse;
+            collisionIndex ++;
+            numCollisions --;
+        }
+    
+    if (collisionIndex >= 1000) {
+        collisionTimes.splice(0, collisionIndex);
+        collisionIndex = 0;
     }
+
     ctx.clearRect(0,0,canvas.width, canvas.height);
     for (const b of molecules) {
         drawBall(b);
@@ -147,7 +189,7 @@ function update() {
 }
 
 function getSpeed(ball) {
-    return Math.sqrt(ball.vx**2 + ball.vy**2);
+    return Math.sqrt(ball.vx**2 + ball.vy**2 + ball.vz**2);
 }
 
 function getArea() {
@@ -161,6 +203,16 @@ function calculateTemperature() {
     }
 
     return totalKE * 2 / (DEGREES_OF_FREEDOM * N * BOLTZMANN_CONSTANT);
+}
+
+function calculateRMSFromTemp() {
+    return Math.sqrt(DEGREES_OF_FREEDOM * BOLTZMANN_CONSTANT * curTemp / NITROGEN_MASS);
+}
+
+function changeSpeedToMatchTemp(ball, tempRatio) {
+    ball.vx *= Math.sqrt(tempRatio);
+    ball.vy *= Math.sqrt(tempRatio);
+    ball.vz *= Math.sqrt(tempRatio);
 }
 
 function drawBall(ball) {
@@ -184,22 +236,34 @@ function updateWallCollision(ball) {
     if (ball.x - ball.radius <= 0) {
         ball.x = ball.radius - (ball.x - ball.radius);
         ball.vx = - ball.vx;
-        totalImpulse += 2 * ball.mass * Math.abs(ball.vx);
+        let impulse = 2 * ball.effMass * Math.abs(ball.vx);
+        totalImpulse += impulse;
+        numCollisions ++;
+        collisionTimes.push({curTime, impulse});
     }
     if (ball.x + ball.radius >= canvas.width) {
         ball.x = ball.x - 2 * (ball.x + ball.radius - canvas.width);
         ball.vx = -ball.vx;
-        totalImpulse += 2 * ball.mass * Math.abs(ball.vx);
+        let impulse = 2 * ball.effMass * Math.abs(ball.vx);
+        totalImpulse += impulse;
+        numCollisions ++;
+        collisionTimes.push({curTime, impulse});
     }
     if (ball.y - ball.radius <= getTopWall()) {
         ball.y = ball.y - 2 * (ball.y - ball.radius - getTopWall());
         ball.vy = -ball.vy;
-        totalImpulse += 2 * ball.mass * Math.abs(ball.vy);
+        let impulse = 2 * ball.effMass * Math.abs(ball.vy);
+        totalImpulse += impulse;
+        numCollisions ++;
+        collisionTimes.push({curTime, impulse});
     }
     if (ball.y + ball.radius >= canvas.height) {
         ball.y = ball.y - 2 * (ball.y + ball.radius - canvas.height);
         ball.vy = - ball.vy;
-        totalImpulse += 2 * ball.mass * Math.abs(ball.vy);
+        let impulse = 2 * ball.effMass * Math.abs(ball.vy);
+        totalImpulse += impulse;
+        numCollisions ++;
+        collisionTimes.push({curTime, impulse});
     }
 }
 
@@ -259,7 +323,7 @@ function getPerimeter() {
 }
 
 function getPressure() {
-    return totalImpulse / (curSeconds * getPerimeter());
+    return totalImpulse / (SECONDS_PER_WINDOW * getPerimeter());
 }
 
 function getRMSSpeed(intervals,) {
@@ -386,7 +450,6 @@ function getTopWall() {
 }
 
 function drawContainer() {
-
     topWall = getTopWall();
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,canvas.width, topWall);
@@ -406,6 +469,48 @@ heightSlider.addEventListener("input", function () {
         `${heightFactor.toFixed(2)}x`;
     }
 );
+
+moleSlider.addEventListener("input", function () {
+    newN = Number(moleSlider.value);
+    if (newN > N) {
+        addMolecule(newN-N);
+    }
+    else if (N > newN) {
+        removeMolecule(N-newN);
+    }
+    N = newN;
+    moleNumberInput.value = N;
+});
+
+
+
+moleNumberInput.addEventListener("change", function () {
+    newN = Number(moleNumberInput.value);
+    if (newN > N) {
+        addMolecule(newN-N);
+    }
+    else if (N > newN) {
+        removeMolecule(N-newN);
+    }
+    N = newN;
+    moleSlider.value = N;
+})
+
+tempSlider.addEventListener("input", function () {
+    newTemp = Number(tempSlider.value);
+    for (const b of molecules) {
+        changeSpeedToMatchTemp(b, newTemp / curTemp);
+    }
+    tempNumberInput.value = newTemp;
+});
+
+tempNumberInput.addEventListener("change", function () {
+    newTemp = Number(tempNumberInput.value);
+    for (const b of molecules) {
+        changeSpeedToMatchTemp(b, newTemp / oldTemp);
+    }
+    tempSlider.value = newTemp;
+})
 
 pausePlayButton.addEventListener("click", function () {
     isPaused = !isPaused;
